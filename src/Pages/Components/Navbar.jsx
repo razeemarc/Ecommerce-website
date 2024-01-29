7776439bb9fadd667480af3e30c1fe3ff706b292
razeemarc <razeemarc@gmail.com>
2024-01-29 12:57:13 +0530

    Navbar UI updated

--- src/Pages/Components/Navbar.jsx
+++ src/Pages/Components/Navbar.jsx
@@ -1,46 +1,62 @@
 import { useRef } from "react";
-import { FaBars, FaTimes,FaRegHeart,FaShoppingCart } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "../Styles/Navbar.css";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from "react-router-dom";
+import { faShoppingCart, faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Navbar() {
-	const navRef = useRef();
-	const navigate = useNavigate();
-	const showNavbar = () => {
-		navRef.current.classList.toggle(
-			"responsive_nav"
-		);
-	};
+  const navRef = useRef();
+  const navigate = useNavigate();
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-	return (
-		<header>
-			<span style ={{fontSize:'20px',marginLeft:'8px'}}> SHINE</span>
-			<nav ref={navRef}>
-				<a href="/home">Home</a>
-				<a href="/shop">Shop</a>
-				<a href="/#">About</a>
-				<a href="/#">Contact us</a>
-				<button
-					className="nav-btn nav-close-btn"
-					onClick={showNavbar}>
-					<FaTimes />
-				</button>
-				
-			</nav>
-			<div className="iconsStyle">
-        <FaRegHeart className="icons" />
-        <FaShoppingCart className="icons" onClick={()=>navigate('/cart')} />		
+  return (
+    <header>
+      <span
+        style={{ fontSize: "20px", marginLeft: "8px", cursor: "pointer" }}
+        onClick={() => navigate("/")}
+      >
+        {" "}
+        SHINE
+      </span>
+      
+      <nav ref={navRef}>
+    
+        <a  href="/home" className="navbarMenu">Home</a>
+        <a href="/shop" className="navbarMenu2" >Shop</a>
+        <a href="/#"className="navbarMenu3">About</a>
+        <a href="/#"className="navbarMenu4">Contact us</a>
+        
+        <button  className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <div className="iconsStyle">
+        <Link
+          activeClass="active"
+          to="/wishlist"
+          className="desktopMenuListItem"
+        >
+          <FontAwesomeIcon icon={faHeart} />{" "}
+          <span className="hide-on-small-screen">Wishlist</span>
+        </Link>
+        <Link activeClass="active" to="/cart" className="desktopMenuListItem">
+          <FontAwesomeIcon icon={faShoppingCart} />{" "}
+          <span className="hide-on-small-screen">Cart</span>
+        </Link>
       </div>
-			
-	<button onClick={()=>navigate("/login")} className="btn-login"> LOG IN </button>
-			
-			<button
-				className="nav-btn "
-				onClick={showNavbar}>
-				<FaBars />
-			</button>
-		</header>
-	);
+      <button onClick={() => navigate("/login")} className="btn-login">
+        {" "}
+        LOG IN{" "}
+      </button>
+
+      <button className="nav-btn " onClick={showNavbar}>
+        <FaBars />
+      </button>
+    </header>
+  );
 }
 
 export default Navbar;
